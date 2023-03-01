04f24603e535e1b5661e34470b07cd79e5c99b32
Leonardo Colaleo <124148340+leon94H@users.noreply.github.com>
2023-03-01 18:15:59 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,102 @@
+<!-- 
+Dato un array di numeri, scrivere un programma che mi permetta di avere la media SOLO dei numeri pari
+contenuti all’interno dell’array -->
+
+
+
+<?php 
+
+//   $arrays = [1,2,3,4,5,6,7,8,9,10];
+//   $elPari = 0;
+
+//    foreach ($arrays as $values){
+//     if($values %2==0){
+     
+//          $elPari = $elPari+1;
+//          $arrMedia[] = $values;
+    
+//     }
+//  }
+
+//    $sum = array_sum($arrMedia);
+//    $media = $sum/$elPari;
+
+//    echo $media;
+
+
+// Dato un array di utenti con nome, cognome e genere, per ogni utente stampare
+// “Buongiorno Sig. Nome Cognome” o “Buongiorno Sig.ra Nome Cognome” a seconda del genere
+
+
+
+
+// $utenti = [[
+
+//     'nome'=> 'Leonardo',
+//     'cognome'=> 'Colaleo',
+//     'genere' => 'M'
+// ],
+// [   
+    
+//     'nome'=> 'Jennifer',
+//     'cognome'=> 'Maccari',
+//     'genere' => 'F'
+// ],
+// [
+
+//     'nome'=> 'Giulia',
+//     'cognome'=> 'Quazimi',
+//     'genere' => 'F'
+// ],
+// [
+
+//     'nome'=> 'Walter',
+//     'cognome'=> 'Spagnolo',
+//     'genere' => 'M'
+// ]
+// ];
+
+// foreach ($utenti as $utente) {
+   
+//     if ($utente["genere"] == "M") {
+//         echo "Buongiorno Sig. $utente[nome] $utente[cognome] \n";
+//     } else {
+//         echo "Buongiorno Sig.ra $utente[nome] $utente[cognome] \n";
+//     }
+// }
+
+
+
+// Scrivere un programma che stampi in console tutti i numeri da uno a cento. Se il numero è multiplo di 3, stampare “PHP” al posto del numero;
+// se multiplo di 5 deve stampare “JAVASCRIPT”; se multiplo di 3 e 5 (15) deve stampare “HACKADEMY62".
+
+// $multiplo = 3;
+
+// $mutiplo2 = 5;
+
+
+// for($i=1;$i<=100;$i++){
+
+//     if($i %15==0){
+
+//         echo"HACKADEMY67\n";
+//     }
+    
+//     else if ($i %3==0){
+
+//         echo"PHP \n";
+//     }
+//     else if($i %5==0){
+
+//         echo"JAVASCRIPT \n";
+//     }else{
+
+//         echo "$i\n";
+//     }
+
+    
+
+
+}
+
+
